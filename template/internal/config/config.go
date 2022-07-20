0471c0fcbb429f1c06cd7ec01a605503197e4631
razielsd <zrandom@mail.ru>
2022-07-20 19:41:56 +0300

    fix default config

--- template/internal/config/config.go
+++ template/internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	LogLevel string `env:"LOG_LEVEL" envDefault:"info"`
-	GRPCAddr string `env:"GRPC_ADDR" envDefault:":9099"`
-	HTTPAddr string `env:"HTTP_ADDR" envDefault:":8099"`
-	APIAddr  string `env:"API_ADDR" envDefault:":9010"`
+	GRPCAddr string `env:"GRPC_ADDR" envDefault:"0.0.0.0:9099"`
+	HTTPAddr string `env:"HTTP_ADDR" envDefault:"0.0.0.0:8099"`
+	APIAddr  string `env:"API_ADDR" envDefault:"0.0.0.0:9010"`
 }
